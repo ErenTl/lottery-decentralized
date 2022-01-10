02d1e7b61b4218b73f41762ff13bfa894d778fe8
ErenTl <beneyim@gmail.com>
2022-01-11 01:53:51 +0300

    made the code compilable and brownie-config.yaml

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,10 +1,25 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.6;
 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
 contract Lottery {
+    using SafeMathChainlink for uint256;
+
     address payable[] public players;
+    uint256 public entryFeeUSD;
+    AggregatorV3Interface public priceFeedETHUSD;
+
+    constructor(address _priceFeedAddress) public {
+        entryFeeUSD = 10 * (10**18);
+        priceFeedETHUSD = AggregatorV3Interface(_priceFeedAddress);
+        owner = msg.sender;
+    }
 
     function enter() public payable {
-        //$50 minimum
+        //$10 minimum
         players.push(msg.sender);
     }
 
