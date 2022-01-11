465cb3b9083cb8c1451836d7103ac40b2668aa6b
ErenTl <beneyim@gmail.com>
2022-01-11 19:29:16 +0300

    entranceFee and getLatestPrice in Lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -9,12 +9,19 @@ contract Lottery {
     using SafeMathChainlink for uint256;
 
     address payable[] public players;
+    address public owner;
     uint256 public entryFeeUSD;
     AggregatorV3Interface public priceFeedETHUSD;
+    uint256 decimals;
 
-    constructor(address _priceFeedAddress) public {
-        entryFeeUSD = 10 * (10**18);
+    constructor(
+        address _priceFeedAddress,
+        uint256 _decimals,
+        uint256 _entryFeeUSD
+    ) public {
+        entryFeeUSD = _entryFeeUSD * (10**18);
         priceFeedETHUSD = AggregatorV3Interface(_priceFeedAddress);
+        decimals = _decimals;
         owner = msg.sender;
     }
 
@@ -23,9 +30,26 @@ contract Lottery {
         players.push(msg.sender);
     }
 
-    function getEntranceFee() public view returns (uint256) {}
+    function getEntranceFee() public view returns (uint256) {
+        uint256 eth_usd_price = getLatestPrice();
+        // 10/4000
+        uint256 payToEnter = (entryFeeUSD * 10**18) / eth_usd_price;
+        return payToEnter;
+    }
 
     function startLottery() public {}
 
     function endLottery() public {}
+
+    function getLatestPrice() public view returns (uint256) {
+        (
+            uint80 roundID,
+            int256 price,
+            uint256 startedAt,
+            uint256 timeStamp,
+            uint80 answeredInRound
+        ) = priceFeedETHUSD.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * ((18 - decimals)**10); //18 decimals
+        return adjustedPrice;
+    }
 }
